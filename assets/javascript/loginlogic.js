13da6dea9e19538fd74e309c9200ae7b0a07f919
Meanderthal00 <meanderthal00@yahoo.com>
2017-12-19 05:44:37 -0500

    drop down menu added to navigation, code cleaned up/beautified

--- assets/javascript/loginlogic.js
+++ assets/javascript/loginlogic.js
@@ -1,8 +1,8 @@
 // modal command
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     $('.modal').modal();
-  });
+});
 
 var config = {
     apiKey: "AIzaSyD9zUxSiYvAJ5aS_EhGLIx_MWILBbJy4TY",
@@ -20,11 +20,11 @@ var user;
 
 $("#googleBtn").on("click", function (event) {
     event.preventDefault();
-    
 
-    
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
@@ -32,7 +32,7 @@ $("#googleBtn").on("click", function (event) {
         console.log(user.displayName + " is signed in.");
         console.log(user);
         // ...
-      }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -41,13 +41,13 @@ $("#googleBtn").on("click", function (event) {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-      });
+    });
 
-      firebase.auth().onAuthStateChanged(user => {
-        if(user) {
-          window.location = 'landing.html'; //After successful login, user will be redirected to home.html
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            window.location = 'landing.html'; //After successful login, user will be redirected to home.html
         }
-      });
+    });
 
 
 });
